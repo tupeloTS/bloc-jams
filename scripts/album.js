35d8fd216406a33a458f0688d05d0cd47d8c4803
Ty Smith <tycon14@att.net>
2015-09-22 14:45:21 -0500

    checkpoint 26

--- scripts/album.js
+++ scripts/album.js
@@ -49,7 +49,7 @@
      
      var template =
         '<tr class="album-view-song-item">'
-      + '  <td class="song-item-number">' + songNumber + '</td>'
+        + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
       + '  <td class="song-item-title">' + songName + '</td>'
       + '  <td class="song-item-duration">' + songLength + '</td>'
       + '</tr>'
@@ -84,9 +84,33 @@ var setCurrentAlbum = function(album) {
  
  };
  
+var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+var songRows = document.getElementsByClassName('album-view-song-item');
+ 
+ // Album button templates
+
+var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
  window.onload = function() {
    
      setCurrentAlbum(albumPicasso);
+     
+     for (i = 0; i < songRows.length; i++) {
+         songRows[i].addEventListener('mouseleave', function(event) {
+             // Revert the content back to the number
+              // Selects first child element, which is the song-item-number element
+             this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+         });
+     }
+
+     
+     songListContainer.addEventListener('mouseover', function(event) {
+         // #1
+          // Only target individual song rows during event delegation
+     if (event.target.parentElement.className === 'album-view-song-item') {
+        event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+     }
+     });
+
    var albums = [albumPicasso, albumMarconi, albumLevon];
    var i = 1; 
 
