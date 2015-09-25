be5c1a454b66ed019cfd9862622ce57807096d1a
Ty Smith <tycon14@att.net>
2015-09-25 15:20:25 -0500

    checkpoint 26 beatdown

--- scripts/album.js
+++ scripts/album.js
@@ -83,13 +83,66 @@ var setCurrentAlbum = function(album) {
      }
  
  };
+ var findParentByClassName = function(element, targetClass) {
+    
+    var currentParent = element.parentElement;
+    
+    while (currentParent.className != targetClass) {
+        currentParent = currentParent.parentElement;
+    }
+    
+    return currentParent;
+
+};
+
+var getSongItem = function(element) {
+    
+    switch (element.className) {
+        case 'album-song-button':
+        case 'ion-play':
+        case 'ion-pause':
+            return findParentByClassName(element, 'song-item-number');
+        case 'album-view-song-item':
+            return element.querySelector('.song-item-number');
+        case 'song-item-title':
+        case 'song-item-duration':
+            return findParentByClassName(element, 'album-view-song-item').querySelector('.song-item-number');
+        case 'song-item-number':
+            return element;
+        default:
+            return;
+    }
+    
+};
+
+ var clickHandler = function(targetElement) {
+     var songItem = getSongItem(targetElement);  
+ if (currentlyPlayingSong === null) {
+         songItem.innerHTML = pauseButtonTemplate;
+         currentlyPlayingSong = songItem.getAttribute('data-song-number');
+     } else if (currentlyPlayingSong === songItem.getAttribute('data-song-number')) {
+         songItem.innerHTML = playButtonTemplate;
+         currentlyPlayingSong = null;
+     } else if (currentlyPlayingSong !== songItem.getAttribute('data-song-number')) {
+         var currentlyPlayingSongElement = document.querySelector('[data-song-number="' + currentlyPlayingSong + '"]');
+         currentlyPlayingSongElement.innerHTML = currentlyPlayingSongElement.getAttribute('data-song-number');
+         songItem.innerHTML = pauseButtonTemplate;
+         currentlyPlayingSong = songItem.getAttribute('data-song-number');
+     }
+
  
+ };
 var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
 var songRows = document.getElementsByClassName('album-view-song-item');
- 
+
  // Album button templates
 
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
+
+ // Store state of playing songs
+ var currentlyPlayingSong = null;
+
  window.onload = function() {
    
      setCurrentAlbum(albumPicasso);
@@ -97,9 +150,19 @@ var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></
      for (i = 0; i < songRows.length; i++) {
          songRows[i].addEventListener('mouseleave', function(event) {
              // Revert the content back to the number
-              // Selects first child element, which is the song-item-number element
-             this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+              var songItem = getSongItem(event.target);
+             var songItemNumber = songItem.getAttribute('data-song-number');
+ 
+             // #2
+             if (songItemNumber !== currentlyPlayingSong) {
+                 songItem.innerHTML = songItemNumber;
+             }
          });
+         
+         songRows[i].addEventListener('click', function(event) {
+             clickHandler(event.target);
+         });
+
      }
 
      
@@ -108,9 +171,15 @@ var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></
           // Only target individual song rows during event delegation
      if (event.target.parentElement.className === 'album-view-song-item') {
         event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+            var songItem = getSongItem(event.target);
+
+            if (songItem.getAttribute('data-song-number') !== currentlyPlayingSong) {
+                songItem.innerHTML = playButtonTemplate;
+            }
      }
      });
-
+     
+     
    var albums = [albumPicasso, albumMarconi, albumLevon];
    var i = 1; 
 
