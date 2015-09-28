c53b105977646602594c06727f684594e1a21ea5
Ty Smith <tycon14@att.net>
2015-09-28 15:42:15 -0500

    CP 28 with w problems

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,35 +1,36 @@
 
  
-var pointsArray = document.getElementsByClassName('point');
+var animatePoints = function() {
                    
-var revealPoint = function(point) {
-    point.style.opacity = 1;
-    point.style.transform = "scaleX(1) translateY(s)";
-    point.style.msTransform = "scaleX(1) translateY(0)";
-    point.style.WebkitTransform = "scaleX(1) translateY(0)";   
+
+    var revealPoint = function() {
+         // #7
+         $(this).css({
+             opacity: 1,
+             transform: 'scaleX(1) translateY(0)'
+         });
     };
 
-var animatePoints = function(points) {                                
-    forEach(points, revealPoint);
+   $.each($('.point'), revealPoint);                            
+     // forEach(points, revealPoint);
 };
 
-window.onload = function() {
+$(window).load(function() {
     
-    if (window.innerHeight > 950) {
-         animatePoints(pointsArray);
+    if ($(window).height() > 950 {
+        animatePoints();
      }
     
-    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = $('.selling-points').offset().top -$(window).height + 200;
     
-    window.addEventListener('scroll', function(event) {
+    $(window).scroll(function(event) {
         console.log(event);
          
-    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
-    if (document.body.scrollTop >= scrollDistance) {
-        animatePoints(pointsArray);
-    }
-     });
- 
-}
+    
+    if ($(window).scrollTop() >= scrollDistance) {
+             animatePoints();
+      }
+  }); 
+});
                
                 
