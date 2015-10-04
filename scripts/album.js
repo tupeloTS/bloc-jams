6fb9149e705502e6a21b36c934b209f3f8f2e7f7
Ty Smith <tycon14@att.net>
2015-10-04 16:58:18 -0500

    CP and lesson 32

--- scripts/album.js
+++ scripts/album.js
@@ -64,11 +64,7 @@ var createSongRow = function(songNumber, songName, songLength) {
 
     };
     
-    var clickHandler = function() {
-         // clickHandler logic
-     };
- 
-    
+     
     var onHover = function(event) {
         var songNumberCell = $(this).find('.song-item-number');
         var songNumber = songNumberCell.attr('data-song-number');
@@ -85,12 +81,11 @@ var createSongRow = function(songNumber, songName, songLength) {
         if (songNumber !== currentlyPlayingSong) {
             songNumberCell.html(songNumber);
         }
-};
-        $row.find('.song-item-number').click(clickHandler);
-     // #2
-     $row.hover(onHover, offHover);
-     // #3
-     return $row; 
+    };
+    
+    $row.find('.song-item-number').click(clickHandler)
+    $row.hover(onHover, offHover);
+    return $row; 
 };
 
 var setCurrentAlbum = function(album) {
